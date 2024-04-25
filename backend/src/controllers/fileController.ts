1dc23e27b927052688d9e02d316ffcda2573340b
amanuel tegegne <amanuel0924@gmail.com>
2024-04-25 15:09:38 +0300

    update file future added

--- backend/src/controllers/fileController.ts
+++ backend/src/controllers/fileController.ts
@@ -36,3 +36,24 @@ export const  getAllFile= async (req: Request, res: Response) => {
         return res.status(500).json({ error: error.message });
     }
 }
+
+export const updateFile= async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { name, size, type, description }:IFileInput = req.body;
+
+        const file:File|null = await File.findByPk(id);
+        if (!file) {
+            return res.status(404).json({ error: "File not found" });
+        }
+        file.name = name;
+        file.size = size;
+        file.type = type;
+        file.description = description;
+        await file.update({ name, size, type, description });
+        return res.status(200).json(file);
+    }
+    catch (error:any) {
+        return res.status(500).json({ error: error.message });
+    }
+}
--- backend/src/routes/fileRoutes.ts
+++ backend/src/routes/fileRoutes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
-import { createFile,getFilesById,getAllFile } from '../controllers/fileController';
+import { createFile,getFilesById,getAllFile,updateFile } from '../controllers/fileController';
 
 const router = express.Router();
 
 router.route('/').post( createFile).get( getAllFile);
-router.route('/:id').get( getFilesById);
+router.route('/:id').get( getFilesById).put( updateFile)
 
 
 export default router;
