711a435d9a51bb3cd5768f4392bf20f461eac171
amanuel tegegne <amanuel0924@gmail.com>
2024-04-26 14:53:34 +0300

    add joi validation in create file

--- backend/src/routes/fileRoutes.ts
+++ backend/src/routes/fileRoutes.ts
@@ -2,10 +2,11 @@ import express from 'express';
 
 import { createFile,getFilesById,getAllFile,updateFile,deleteFile } from '../controllers/fileController';
 import uploader from './../controllers/uploadController'
+import { ValidateSchema,Schema } from '../middleware/vallidator';
 
 const router = express.Router();
 
-router.route('/').post( uploader,createFile).get( getAllFile);
+router.route('/').post( uploader,ValidateSchema(Schema.create),createFile).get( getAllFile);
 router.route('/:id').get( getFilesById).put( uploader,updateFile).delete( deleteFile);
 
 
